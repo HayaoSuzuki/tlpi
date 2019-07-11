94bc9961742ddefddd4e6bbfc6b5fecb81195d7e
Hayao Suzuki <hayao.math@gmail.com>
2019-07-11 19:21:03 +0900

    Rust版のfileio/copy.cを書く

--- chap04/copy/src/main.rs
+++ chap04/copy/src/main.rs
@@ -1,5 +1,25 @@
 use std::io::Write;
-use std::path::Path;
+
+const BUF_SIZE: usize = 8 * 1024;
+
+fn copy<R: ?Sized, W: ?Sized>(reader: &mut R, writer: &mut W) -> std::io::Result<u64>
+where
+    R: std::io::Read,
+    W: std::io::Write,
+{
+    let mut buf = [0; BUF_SIZE];
+    let mut written = 0;
+    loop {
+        let len = match reader.read(&mut buf) {
+            Ok(0) => return Ok(written),
+            Ok(len) => len,
+            Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => continue,
+            Err(e) => return Err(e),
+        };
+        writer.write_all(&buf[..len])?;
+        written += len as u64;
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let mut _args = Vec::new();
@@ -10,11 +30,13 @@ fn main() -> std::io::Result<()> {
 
     if _args.len() != 2 {
         writeln!(std::io::stderr(), "Usage: copy source_file target_file").unwrap();
-        std::process::exit(-1);
     } else {
-        let source_file_path = Path::new(&_args[0]);
-        let target_file_path = Path::new(&_args[1]);
-        std::fs::copy(&source_file_path, &target_file_path)?;
+        // let source_file_path = Path::new(&_args[0]);
+        // let target_file_path = Path::new(&_args[1]);
+        // std::fs::copy(source_file_path, target_file_path)?;
+        let mut source_file = std::fs::File::open(&_args[0])?;
+        let mut target_file = std::fs::File::create(&_args[1])?;
+        copy(&mut source_file, &mut target_file)?;
     }
 
     Ok(())
